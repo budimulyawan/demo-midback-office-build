594c6e75464d57f756ad1ebce088446af894f3cc
Budi Mulyawan <budi.mulyawan@gmail.com>
2017-10-13 09:40:50 +1100

    add send terminal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,65 @@
 import React from "react";
 
 const label = {
-  display: 'inline-block',
+  display: "inline-block",
   fontWeight: 700,
   marginBottom: 0.25
-}
+};
 
 const input = {
   border: "1px solid",
-  borderColor: 'blue',
+  borderColor: "blue",
   marginBottom: 0.25,
-  resize: 'none',
-  width: '100%'
+  resize: "none",
+  width: "100%"
+};
+
+const button = {
+  fontSize: "1em",
+  margin: "1em",
+  padding: "0.25em 1em",
+  border: "2px solid #78BE20",
+  borderRadius: "3px",
+  color: "white",
+  cursor: "pointer",
+  background: "#78BE20"
+};
+
+class Profile extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      firstName: "Michael",
+      lastName: "Smith",
+      email: "michael.smith@gmail.com"
+    };
+  }
+
+  upload() {
+    if (webBrowser) {
+      alert(webBrowser.sendTerminalCommand('N.' + this.state.lastName + '/' + this.state.firstName));
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Profile</h1>
+        <div>
+          <label style={label}>First name</label>
+          <input style={input} type="text" value={this.state.firstName} />
+          <label style={label}>Last name</label>
+          <input style={input} type="text" value={this.state.lastName} />
+          <label style={label}>Email</label>
+          <input style={input} type="email" value={this.state.email} />
+          <button style={button} onClick={this.upload.bind(this)}>
+            Upload
+          </button>
+        </div>
+        <p>Travelport Pacific 2017</p>
+      </div>
+    );
+  }
 }
 
-const button ={
-  fontSize: '1em',
-	margin: '1em',
-	padding: '0.25em 1em',
-	border: '2px solid #78BE20',
-  borderRadius: '3px',
-  color:'white',
-  cursor: 'pointer',
-  background:'#78BE20'
-
-}
-
-export default () => (
-  <div>
-    <h1></h1>
-    <div>
-      <form >
-        <label style={label}>First name</label>
-        <input style={input}ype="text" value='Michael' />
-        <label style={label}>Last name</label>
-        <input style={input} type="text" value='Smith' />
-        <label style={label}>Email</label>
-        <input style={input} type="email" value='michael.smith@gmail.com' />
-        <button style={button}>Upload</button>
-      </form>
-    </div>
-    <p>Travelport Pacific 2017</p>
-  </div>
-);
+export default Profile;
